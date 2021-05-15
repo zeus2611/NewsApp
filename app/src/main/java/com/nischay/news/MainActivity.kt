9290502b633f675cfe7cc73e81e5be3cb0f922f0
Nischay <nischay26.scs@gmail.com>
2021-05-15 20:09:41 +0530

    Initial commit

---
+++ app/src/main/java/com/nischay/news/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.nischay.news
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var recyclerView: RecyclerView = findViewById(R.id.recyclerView)
+    }
+}
